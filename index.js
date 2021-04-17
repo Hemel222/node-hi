e5a64f47dff57f18ed9be19576007580efbd81cb
Hemel <hemel.officialid@gmail.com>
2021-04-17 21:00:23 +0600

    Update index.js

--- index.js
+++ index.js
@@ -1,7 +1,7 @@
 const express = require("express");
 const app = express();
 
-app.get("/", function (res, req){
+app.get("/", function (req, res){
     res.send("subdomain take-over successfully");
 });
 app.listen(process.env.PORT || 8080); 
